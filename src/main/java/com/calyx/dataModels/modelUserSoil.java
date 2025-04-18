d132c71daceb18297d7b0df3dc5d7d10c204adaa
Aimear <Princeamir2002@hotmail.com>
2025-04-18 13:17:50 -0400

    Saving user data

---
+++ src/main/java/com/calyx/dataModels/modelUserSoil.java
@@ -0,0 +1,16 @@
+package com.calyx.dataModels;
+
+import java.io.File;
+
+public class modelUserSoil {
+
+    public modelUserSoil(String PH, String Nitrogen, String Phosphorus, String Potassium) {
+        
+    }
+
+    public void checkUserdata() {
+        if (File. == null) {
+
+        }
+    }
+}
